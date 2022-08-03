f7e1d1cc9ef0614a72fa51aefe5d4170e4b81b5c
agnekauk <agnenemeikaite@gmail.com>
2022-08-03 18:26:49 +0300

    Initial HTML, CSS and JS

---
+++ main.js
@@ -0,0 +1,26 @@
+let btn = document.querySelector(".comment-btn");
+
+let bodyColor = document.querySelector("body");
+
+let formToFill = document.querySelector(".form-to-fill");
+btn.addEventListener("click", function () {
+    formToFill.classList.toggle("visual");
+})
+
+let submit = document.querySelector(".submit-button");
+
+submit.addEventListener("click", function () {
+    swal("Gavome!", "Dėkojame už Jūsų nuomonę!", "success");
+    formToFill.classList.toggle("visual");
+})
+
+let colorBtn = document.querySelector(".color-button");
+let randcol = "";
+let allchar = "0123456789ABCDEF";
+
+colorBtn.addEventListener("click", function () {
+    for (let i = 0; i < 6; i++) {
+        randcol += allchar[Math.floor(Math.random() * 16)];
+    }
+    document.body.style.backgroundColor = "#" + randcol;
+})
